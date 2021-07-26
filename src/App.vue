fdb38693588f136931236a0807ab6e84adbe287e
caiovinics <caiovinics@users.noreply.github.com>
2021-07-26 16:02:52 -0400

    ajustando interface do dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,179 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <a-layout id="components-layout-demo-custom-trigger">
+    <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
+      <div class="logo" />
+        <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
+          <a-menu-item key="1">
+            <a-icon type="home" />
+            <span>Página Principal</span>
+          </a-menu-item>
+          <a-menu-item key="2">
+            <a-icon type="user" />
+            <span>Perfil</span>
+          </a-menu-item>
+          <a-menu-item key="3">
+            <a-icon type="setting" />
+            <span>Config.</span>
+          </a-menu-item>
+        </a-menu>
+    </a-layout-sider>
+  <a-layout>
+    <a-layout-header style="background: #fff; padding: 0">
+      <a-icon
+        class="trigger"
+        :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+        @click="() => (collapsed = !collapsed)"
+      />
+      Dashboard
+    </a-layout-header>
+    <div class="card-info">
+      <div class="card">
+        <div class="icon">
+          <img src="src\assets\visits.svg" alt="Visitas">
+        </div>
+        <div class="info">
+          <p>Novas Visitas</p>
+          <span>102.400</span>
+        </div>
+      </div>
+      <div class="card">
+        <div class="icon">
+          <img src=".\assets\messages.png" alt="Visitas">
+        </div>
+        <div class="info">
+          <p>Mensagens</p>
+          <span>81.212</span>
+        </div>
+      </div>
+      <div class="card">
+        <div class="icon">
+          ìcone
+        </div>
+        <div class="info">
+          <p>Compras</p>
+          <span>9.280</span>
+        </div>
+      </div>
+      <div class="card">
+        <div class="icon">
+          ìcone
+        </div>
+        <div class="info">
+          <p>Vendas</p>
+          <span>13.600</span>
+        </div>
+      </div>
+    </div>
+    <a-layout-content
+      :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }">
+    </a-layout-content>
+     <line-chart :chartdata="chartData" :options="chartOptions" />
+    <div class="card-chart">
+      <div class="card">
+        <div class="chart">
+          ìcone
+        </div>
+      </div>
+      <div class="card">
+        <div class="chart">
+          ìcone
+        </div>
+      </div>
+      <div class="card">
+        <div class="chart">
+          ìcone
+        </div>
+      </div>
+    </div>
+  </a-layout>
+  </a-layout>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Line } from "vue-chartjs";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  extends: Line,
+  props: {
+    chartdata: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: null
+    }
+  },
+  mounted() {
+    this.renderChart(this.chartdata, this.options);
   }
-}
+};
+
 </script>
 
 <style>
+.small {
+  max-width: 600px;
+  margin: 150px auto;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+#components-layout-demo-custom-trigger .trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+#components-layout-demo-custom-trigger .trigger:hover {
+  color: #1890ff;
+}
+
+.card-info {
+  display: flex;
+  padding: 11px;
+}
+
+.card {
+  display: flex;
+  justify-content: space-between;
+  background: #fff;
+  width: 300px;
+  margin: 5px;
+  padding: 20px;
+}
+
+.card p {
+  font-weight: 400;
+  color: #27303b;
+}
+
+.card span {
+  font-size: 18px;
+  font-weight: 600;
+}
+
+.card-chart {
+  display: flex;
+  padding: 11px;
+}
+
+.card-chart .card {
+  display: flex;
+  justify-content: space-between;
+  background: #fff;
+  width: 400px;
+  height: 300px;
+  margin: 5px;
+  padding: 20px;
+}
+
 </style>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,45 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: {
+    label: {
+      type: String
+    },
+    chartData: {
+      type: String
+    },
+    options: {
+      type: Object
+    },
+    chartColors: {
+      type: Object
+    }
+  },
+  mounted() {
+    
+    const {
+      borderColor,
+      pointBorderColor,
+      pointBackgroundColor,
+      backgroundColor
+    } = this.chartColors;
+
+    this.renderChart(
+      {
+        datasets: [
+          {
+            data: [13, 31, 31, 43, 31, 98],
+            borderColor: borderColor,
+            pointBorderColor: pointBorderColor,
+            pointBackgroundColor: pointBackgroundColor,
+            backgroundColor: backgroundColor
+          }
+        ]
+      },
+      this.options
+    );
+  }
+};
+</script>
